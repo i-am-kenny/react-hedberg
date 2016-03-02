50844df53943eceb96c8280bd948bb227952debe
kenny mcgarvey <kenny.mcgarvey@gmail.com>
2016-03-01 22:09:55 -0600

    Changes module to use createClass
    Signed-off-by: kenny mcgarvey <kenny.mcgarvey@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var React = require('react'),
 		mitch = require('hedberg');
 
-module.exports = React.Component.extend({
+module.exports = React.createClass({
 	render: function() {
 		return (<span {...this.props}>{mitch(this.props.value)}</span>);
 	}
